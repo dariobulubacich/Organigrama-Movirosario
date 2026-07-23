7fb301e2752fae53849b5c9980128c96cf1691f7
dario <dariobulubacich>
2026-07-22 22:21:08 -0300

    Agrega organigrama estructural6

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Organigrama from "./Organigrama/Organigrama";
 import OrganigramaEstructural from "./pages/OrganigramaEstructural";
 import "./App.css";
+import NodoEstructural from "./organigrama/components/NodoEstructural";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
           path="/organigrama-estructural"
           element={<OrganigramaEstructural />}
         />
+        <Route path="/nodoestructural" element={<NodoEstructural />} />{" "}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
