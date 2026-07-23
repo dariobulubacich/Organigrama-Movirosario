5441bc49fd2bd85532bf8d5718d92a339fad0fb0
dario <dariobulubacich>
2026-07-22 22:11:53 -0300

    prueba 6

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Organigrama from "./Organigrama/Organigrama";
 import OrganigramaEstructural from "./pages/OrganigramaEstructural";
 import "./App.css";
+import NodoEstructural from "./organigrama/components/NodoEstructural";
 
 export default function App() {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Organigrama />} />
-
         <Route
           path="/organigrama-estructural"
           element={<OrganigramaEstructural />}
         />
-
+        <Route path="/nodoestructural" element={<NodoEstructural />} />{" "}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
